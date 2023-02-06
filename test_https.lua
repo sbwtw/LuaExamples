5af7a7d448841dd409d9610564692a92a2ca85bd
石博文 <sbw@sbw.so>
2023-02-07 00:29:08 +0800

    add https example

--- test_https.lua
+++ test_https.lua
@@ -1,3 +1,15 @@
-require 'ssl.https'
+local ltn12 = require 'ltn12'
+local https = require 'ssl.https'
 
-print('asd')
+local resp = {}
+local r, c, h, s = https.request {
+    url = "https://www.google.com",
+    sink = ltn12.sink.table(resp)
+}
+
+print(r, c, h, s)
+print(table.concat(resp))
+
+-- for k, v in pairs(h) do
+--     print(k, v)
+-- end
